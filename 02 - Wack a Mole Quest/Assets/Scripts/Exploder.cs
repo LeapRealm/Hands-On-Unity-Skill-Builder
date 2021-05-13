6493454cb5505e798ed9eab68424471c21659f08
Jinwoo, Kim <admin@leaprealm.com>
2021-05-13 13:35:20 +0900

    Wack a Mole Quest - Gotcha

--- 02 - Wack a Mole Quest/Assets/Scripts/Exploder.cs
+++ 02 - Wack a Mole Quest/Assets/Scripts/Exploder.cs
@@ -2,9 +2,14 @@
 
 public class Exploder : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem catchParticleSource;
+
     private void OnTriggerEnter(Collider other)
     {
         print("Aww, you got me");
+        ParticleSystem catchParticle = Instantiate(catchParticleSource, GetComponentsInChildren<Transform>()[1].position, Quaternion.identity);
+        catchParticle.Play();
+        
         Destroy(gameObject);
     }
 }
