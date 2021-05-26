e65923b5f2ab13309fb77d7313a19974c2737d1f
Jinwoo, Kim <admin@leaprealm.com>
2021-05-26 23:27:01 +0900

    Grapple Gun Quest - Connect The Joint

---
+++ 06 - Grapple Gun Quest/Assets/Scripts/GrapplingHook.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class GrapplingHook : MonoBehaviour
+{
+    [SerializeField] private float distance = 10f;
+    [SerializeField] private LayerMask mask;
+    [SerializeField] private LineRenderer line;
+    [SerializeField] private float grappleSpeed = 3f;
+    [SerializeField] private GameObject playerHand;
+
+    private DistanceJoint2D joint;
+    private Vector3 targetPos;
+    private RaycastHit2D hit;
+
+    private void Start()
+    {
+        joint = GetComponent<DistanceJoint2D>();
+        joint.enabled = false;
+        line.enabled = false;
+    }
+
+    private void Update()
+    {
+        PullPlayer();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPos.z = 0;
+
+            hit = Physics2D.Raycast(playerHand.transform.position, targetPos - playerHand.transform.position, distance, mask);
+
+            if (hit.collider != null && hit.collider.gameObject.GetComponent<Rigidbody2D>() != null)
+            {
+                joint.enabled = true;
+                joint.connectedBody = hit.collider.GetComponent<Rigidbody2D>();
+                
+                //Challenge 3:
+
+                joint.distance = Vector2.Distance(playerHand.transform.position, hit.point);
+
+                line.enabled = true;
+                line.SetPosition(0, playerHand.transform.position);
+                line.SetPosition(1, hit.point);
+            }
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            line.SetPosition(0, playerHand.transform.position);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            joint.enabled = false;
+            line.enabled = false;
+        }
+    }
+
+    private void PullPlayer()
+    {
+        //Challenge 2:  
+    }
+}
