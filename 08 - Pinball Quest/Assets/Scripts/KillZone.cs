16d00e2a88cfd96f914255b757cf671c3a79e157
Jinwoo, Kim <admin@leaprealm.com>
2021-06-28 12:38:46 +0900

    Pinball Quest - Reset

---
+++ 08 - Pinball Quest/Assets/Scripts/KillZone.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) 
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
