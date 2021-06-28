da1e6e70ad6b2430bd26b11487d5f5bf66395f98
Jinwoo, Kim <admin@leaprealm.com>
2021-06-28 12:32:13 +0900

    Pinball Quest - Left And Right

--- 08 - Pinball Quest/Assets/Scripts/Flipper.cs
+++ 08 - Pinball Quest/Assets/Scripts/Flipper.cs
@@ -1,19 +1,41 @@
 using UnityEngine;
 
+public enum Location
+{
+    None,
+    Left,
+    Right
+}
+
 public class Flipper : MonoBehaviour
 {
+    [SerializeField] private Location myLocation;
     private HingeJoint2D joint;
 
     private void Start()
     {
+        if (myLocation == Location.None)
+            Debug.LogError("Location Enum is None");
+        
         joint = GetComponent<HingeJoint2D>();
     }
     
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
-            joint.useMotor = true;
-        else
-            joint.useMotor = false;
+        switch (myLocation)
+        {
+            case Location.Left:
+                if (Input.GetKey(KeyCode.Z))
+                    joint.useMotor = true;
+                else
+                    joint.useMotor = false;
+                break;
+            case Location.Right:
+                if (Input.GetKey(KeyCode.X))
+                    joint.useMotor = true;
+                else
+                    joint.useMotor = false;
+                break;
+        }
     }
 }
