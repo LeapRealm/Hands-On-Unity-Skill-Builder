028880b2a3d7ca04c4103ca937f5ae74e9f32379
Jinwoo, Kim <admin@leaprealm.com>
2021-06-29 22:56:06 +0900

    Pinball Quest - Slow It Down

--- 08 - Pinball Quest/Assets/Scripts/SlowDown.cs
+++ 08 - Pinball Quest/Assets/Scripts/SlowDown.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 
 public class SlowDown : MonoBehaviour
 {
-    // stop. hammer time.
+    [SerializeField] private bool beSlow;
+
+    private void Update()
+    {
+        if (beSlow)
+            Time.timeScale = 0.5f;
+        else
+            Time.timeScale = 1.0f;
+    }
 }
