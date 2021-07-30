a4391cd15a651367cd4bfda3ef4bcb0e89e0ddad
JinwooKim <jinwookim@leaprealm.com>
2021-07-30 09:26:11 +0900

    Pinball Quest  - Teleportation

--- 08 - Pinball Quest/Assets/Scripts/Teleport.cs
+++ 08 - Pinball Quest/Assets/Scripts/Teleport.cs
@@ -2,5 +2,17 @@ using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
-    // hint: we did teleporting in one of the early Quests.
+    [SerializeField] private Transform outTransform;
+    [SerializeField] private float force;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        Rigidbody2D playerRigidbody2D = other.GetComponent<Rigidbody2D>();
+        playerRigidbody2D.velocity = Vector2.zero;
+        playerRigidbody2D.transform.position = outTransform.position;
+        playerRigidbody2D.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+    }
 }
